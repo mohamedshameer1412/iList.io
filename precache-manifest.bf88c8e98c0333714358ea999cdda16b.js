04670f64867bbcefe4b0a2c664ffa093616f9efc
mohamedshameer1412 <75442892+mohamedshameer1412@users.noreply.github.com>
2024-04-04 07:00:50 +0530

    Updates

---
+++ precache-manifest.bf88c8e98c0333714358ea999cdda16b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253c827becf85690782345de22198151",
+    "url": "/iList.io/index.html"
+  },
+  {
+    "revision": "47ec1615daa6ae6db1cd",
+    "url": "/iList.io/static/css/main.36ccb835.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/iList.io/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/iList.io/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "47ec1615daa6ae6db1cd",
+    "url": "/iList.io/static/js/main.2d45894b.chunk.js"
+  },
+  {
+    "revision": "32b1f1b0b8dfd895507a",
+    "url": "/iList.io/static/js/runtime~main.76251c47.js"
+  }
+]);
